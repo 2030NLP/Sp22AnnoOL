503c66e5f5770be8d7cfc791b2e6efd0269e83a5
Roomcar <noteforziio@163.com>
2022-06-08 20:32:01 +0800

    save

--- js/master_constants.mjs.js
+++ js/master_constants.mjs.js
@@ -1,5 +1,5 @@
 // 基本信息 变量
-export const APP_VERSION = "22-0608-1845";
+export const APP_VERSION = "22-0608-2030";
 
 // 开发环境 和 生产环境 的 控制变量
 export const DEVELOPING = location?.hostname=="2030nlp.github.io" ? 0 : 1;
