4e916d9b0bd5558393c22ab48ce4cb3c26733877
CH Sun <noteforziio@163.com>
2023-03-04 15:48:13 +0800

    修改后端接口

--- js/master_constants.mjs.js
+++ js/master_constants.mjs.js
@@ -14,7 +14,7 @@ export const API_BASE_DEV_LOCAL = "http://127.0.0.1:5500/";
 export const DEV_HOSTS = ["http://127.0.0.1:5000", "http://127.0.0.1:8888", "http://192.168.1.100:8888", "http://10.1.115.146:8888/", "http://10.0.55.176:8888/", "http://10.2.25.245:8888/"];
 
 export const API_BASE_DEV = DEV_HOSTS[0];
-export const API_BASE_PROD = "https://sp22.nlpsun.cn";
+export const API_BASE_PROD = "https://www.lmxiao.cn";
 export const API_BASE = DEVELOPING ? API_BASE_DEV : API_BASE_PROD;
 
 // 10.1.115.146
